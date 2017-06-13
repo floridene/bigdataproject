5fe87199005757cf5982a3bd02a146aae57f867b
vhwr <s_weidmann@stud.hwr-berlin.de>
2017-06-14 00:12:20 +0200

    run transMatrix again

--- markov_chain_bash4.R
+++ markov_chain_bash4.R
@@ -3,7 +3,7 @@ library(dplyr)
 
 load("/home/Vera_Weidmann/Supermarket/00_Data/par_longdata.rda")
 
-longdata<- data.table(longdata)
+longdata<- as.data.frame(longdata)
 transMatrix <- longdata %>% group_by(V1,V2) %>% summarise(n=n()) %>% mutate(p=n/max(n)) %>% dcast(V1 ~ V2, value.var="p")
 
 #transMatrix[as.numeric(is.na(transMatrix))] = 0
