a2f31c0494192a9fb8c7a9a9905d685fa12a8e7c
vhwr <s_weidmann@stud.hwr-berlin.de>
2017-06-13 23:11:31 +0200

    run transMatrix again

--- markov_chain_bash4.R
+++ markov_chain_bash4.R
@@ -5,6 +5,10 @@ load("/home/Vera_Weidmann/Supermarket/00_Data/par_longdata.rda")
 
 longdata<- data.table(longdata)
 transMatrix <- longdata %>% group_by(V1,V2) %>% summarise(n=n()) %>% mutate(p=n/max(n)) %>% dcast(V1 ~ V2, value.var="p")
-transMatrix[is.na(transMatrix)] = 0
 
-save(transMatrix, file = "/home/Vera_Weidmann/Supermarket/00_Data/transMatrix.rda")
+#transMatrix[as.numeric(is.na(transMatrix))] = 0
+
+save(transMatrix, file = "/home/Vera_Weidmann/Supermarket/00_Data/1406transMatrix.rda")
+
+
+#-> Problem: just values for products until column 15000
